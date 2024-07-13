b7ef211b98bc4cea5252e6685c1bfc553029d853
Ricardo Giovani Piantavinha Perandré <ricardo.piantavinha@gmail.com>
2024-07-12 21:45:42 -0300

    React admin backend integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
+import { Admin, Resource, ShowGuesser } from 'react-admin';
+import { UserList, UserCreate, UserEdit } from './components';
+import jsonServerProvider from 'ra-data-json-server';
 
-export const App = () => <Admin></Admin>;
+const dataProvider = jsonServerProvider('http://localhost:3000');
+
+export const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource
+            name="users"
+            list={UserList}
+            edit={UserEdit}
+            create={UserCreate}
+            show={ShowGuesser}
+        />
+    </Admin>
+);
---
+++ src/components.jsx
@@ -0,0 +1,32 @@
+import { Create, Datagrid, EmailField, Edit, List, SimpleForm, TextField, TextInput } from "react-admin";
+
+export const UserList = (data) => (
+    <List>
+        <Datagrid data={data.data} rowClick="edit">
+            <TextField source="id"/>
+            <TextField source="name"/>
+            <EmailField source="email"/>
+            <TextField source="password" type="password"/>
+        </Datagrid>
+    </List>
+);
+
+export const UserEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="name"/>
+            <TextInput source="email"/>
+            <TextInput source="password" type="password"/>
+        </SimpleForm>
+    </Edit>
+);
+
+export const UserCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="name"/>
+            <TextInput source="email"/>
+            <TextInput source="password" type="password"/>
+        </SimpleForm>
+    </Create>
+);
